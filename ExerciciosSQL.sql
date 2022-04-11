02d7186aa05240caba272e3ce29c9e03961b1692
Jardel Silva <jardel.s22@gmail.com>
2022-04-10 21:18:01 -0300

    Update ExerciciosSQL.sql

--- ExerciciosSQL.sql
+++ ExerciciosSQL.sql
@@ -5,10 +5,13 @@ We have a table called Shippers. Return all the fields from all the shippers. */
 SELECT * 
 FROM dbo.Shippers;
 
+
 /* 2. Certain fields from Categories
+
 In the Categories table, selecting all the fields using this SQL:
 SELECT * FROM Categories
 ... will return 4 columns. We only want to see two columns, CategoryName and Description.*/
+
 SELECT CategoryName,
 	Description
 FROM dbo.Categories;
