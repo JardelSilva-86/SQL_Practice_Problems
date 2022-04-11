019450ed5147fd4f71b6d993f752d5405072676a
JardelSilva <53876124+JardelSilva-86@users.noreply.github.com>
2022-04-11 16:49:26 -0300

    Update Exercicio4.sql

--- Exercicio4.sql
+++ Exercicio4.sql
@@ -1,11 +1,9 @@
-/* 4. Sales Representatives in the  United States
+/* Exercise 4 - Sales Representatives in the  United States
 
-Now we'd like to see the same columns as above, but only for those employees that both have the title of Sales Representative, 
-and also are in the United Sates*/
+Now we'd like to see the same columns as above, but only for those employees that both have the title of Sales Representative, and also are in the United Sates. */
 
 SELECT FirstName,
 	LastName,
 	HireDate
 FROM Employees
 WHERE Title = 'Sales Representative' AND Country = 'USA';
-
