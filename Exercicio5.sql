5da8266dd23c29df67d94b955695c498b8efaa25
JardelSilva <53876124+JardelSilva-86@users.noreply.github.com>
2022-04-11 16:50:25 -0300

    Update Exercicio5.sql

--- Exercicio5.sql
+++ Exercicio5.sql
@@ -1,8 +1,8 @@
-/* 5. Orders placed by specific EmployeeID
+/* Exercise 5 - Orders placed by specific EmployeeID
 
 Show all the orders placed by a specific employee. The EmployeeId for this Emloyee (Steve Buchanan) is 5.*/
 
 SELECT OrderID,
-	OrderDate
+  OrderDate
 FROM Orders
 WHERE EmployeeID = 5;
