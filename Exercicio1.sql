eb37efa2773afeed16b6f124163cc27a5a16e844
JardelSilva <53876124+JardelSilva-86@users.noreply.github.com>
2022-04-11 16:47:35 -0300

    Update and rename Exercicios1.sql to Exercicio1.sql

--- Exercicio1.sql
+++ Exercicio1.sql
@@ -1,8 +1,6 @@
-/* 1. Which shippers do we have?
+/* Exercise 1 - Which shippers do we have?
 
 We have a table called Shippers. Return all the fields from all the shippers. */
 
 SELECT TOP (100) * 
 FROM dbo.Shippers;
-
-
