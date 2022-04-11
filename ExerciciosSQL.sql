bf6ee16280701f743fc0e57e62f8ed782fb8e5c0
Jardel Silva <jardel.s22@gmail.com>
2022-04-11 07:27:57 -0300

    Update ExerciciosSQL.sql

--- ExerciciosSQL.sql
+++ ExerciciosSQL.sql
@@ -38,3 +38,12 @@ SELECT FirstName,
 	HireDate
 FROM Employees
 WHERE Title = 'Sales Representative' AND Country = 'USA';
+
+/* 5. Orders placed by specific EmployeeID
+
+Show all the orders placed by a specific employee. The EmployeeId for this Emloyee (Steve Buchanan) is 5.*/
+
+SELECT OrderID,
+	OrderDate
+FROM Orders
+WHERE EmployeeID = 5;
