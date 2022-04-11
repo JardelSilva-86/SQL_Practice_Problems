e6f602524ec56f77075df2150e78747f60d26bb9
JardelSilva <53876124+JardelSilva-86@users.noreply.github.com>
2022-04-11 11:49:48 -0300

    Create Exercicio2.sql

---
+++ Exercicio2.sql
@@ -0,0 +1,10 @@
+/* 2. Certain fields from Categories
+
+In the Categories table, selecting all the fields using this SQL:
+SELECT * FROM Categories
+... will return 4 columns. We only want to see two columns, CategoryName and Description.*/
+
+SELECT CategoryName,
+	Description
+FROM dbo.Categories;
+
