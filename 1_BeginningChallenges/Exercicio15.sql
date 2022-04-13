da4bb40fcdea469f4a669e752ef065291fc2cee6
JardelSilva <53876124+JardelSilva-86@users.noreply.github.com>
2022-04-13 12:02:55 -0300

    Update Exercicio15.sql

--- 1_BeginningChallenges/Exercicio15.sql
+++ 1_BeginningChallenges/Exercicio15.sql
@@ -1,8 +1,6 @@
 /* Exercise 15 - When eas the first order?
 
-Show the data of the first order ever made in the Orders Table.
+Show the data of the first order ever made in the Orders Table. */
 
 SELECT MIN(OrderDate) AS 'FirstOrder'
 FROM Orders;
-
-*/
