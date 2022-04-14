bf3d84c08753a67a50ab53c99cb6fa4d7774b484
Jardel Silva <jardel.s22@gmail.com>
2022-04-13 21:51:35 -0300

    Update Exercicio20.sql

---
+++ 2_IntermediateChallenges/Exercicio20.sql
@@ -0,0 +1,3 @@
+/* Exercise 20 - Categories, and the total products in each category
+
+For this problem, we'd like to see the total number os products in each category. Sort the results by the number of products, in descending order. */
