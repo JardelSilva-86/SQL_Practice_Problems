e5fdd4ac6caf881ea81e357e9b2e11cf0e9f673f
Jardel Silva <jardel.s22@gmail.com>
2022-04-14 07:21:50 -0300

    Update Exercicio20.sql

--- 2_IntermediateChallenges/Exercicio20.sql
+++ 2_IntermediateChallenges/Exercicio20.sql
@@ -1,3 +1,10 @@
 /* Exercise 20 - Categories, and the total products in each category
 
 For this problem, we'd like to see the total number os products in each category. Sort the results by the number of products, in descending order. */
+
+SELECT c.CategoryName,
+    COUNT(p.ProductID) AS 'TotalProducts'
+FROM Categories c JOIN Products p
+ON c.CategoryID = p.CategoryID
+GROUP BY CategoryName
+ORDER BY 'TotalProducts' DESC
---
+++ 2_IntermediateChallenges/Exercicio21.sql
@@ -0,0 +1,3 @@
+/* Exercise 21 - Total customers per country/city
+
+*/
