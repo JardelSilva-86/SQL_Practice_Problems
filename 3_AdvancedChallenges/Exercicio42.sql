7369639043ddb131cf085f22d395562499fd015d
JardelSilva <53876124+JardelSilva-86@users.noreply.github.com>
2022-04-19 15:17:28 -0300

    Update Exercicio42.sql

--- 3_AdvancedChallenges/Exercicio42.sql
+++ 3_AdvancedChallenges/Exercicio42.sql
@@ -1,4 +1,6 @@
-/* Exercise 42 - Some salespeople have more orders arriving late than others.
+/* Exercise 42 - Late orders - which employees? 
+
+Some salespeople have more orders arriving late than others.
 Maybe they're not following up on the order process, and need more training.
 Which salespeople have the most orders arriving late?
 */
