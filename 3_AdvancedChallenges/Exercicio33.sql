39b57cb98af27ee68a8511325c2f194e1efb5875
JardelSilva <53876124+JardelSilva-86@users.noreply.github.com>
2022-04-19 14:10:02 -0300

    Create Exercicio33.sql

---
+++ 3_AdvancedChallenges/Exercicio33.sql
@@ -0,0 +1,5 @@
+/* Exercise 33 - High-value customers - total orders
+
+The manager has changed his mind. Instead of requiring that customers have at least one individual orders totaling $10,000 or more, he wants to define high-value customers as those who have orders totaling $15,000 or more in 2016.
+How would you change the answer to the problem above?
+*/
