d0e7ea9f55eca3b06c1f2104b1cd07773e8f408f
JardelSilva <53876124+JardelSilva-86@users.noreply.github.com>
2022-04-19 15:21:31 -0300

    Create Exercicio44.sql

---
+++ 3_AdvancedChallenges/Exercicio44.sql
@@ -0,0 +1,5 @@
+/* Exercise 44 - Late orders vs. total orders - missing employee
+
+There's an employee missing in the answer from the problem above.
+Fix the SQL to show all employees who have taken orders.
+*/
