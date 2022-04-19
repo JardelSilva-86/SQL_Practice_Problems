2ec5fa9e1b7771fbc72494544d6f526dbf0fc3f7
JardelSilva <53876124+JardelSilva-86@users.noreply.github.com>
2022-04-19 14:22:25 -0300

    Create Exercicio35.sql

---
+++ 3_AdvancedChallenges/Exercicio35.sql
@@ -0,0 +1,5 @@
+/* Execise 35 - Month-end orders
+
+At the end of the month, salespeople are likely to try much harder to get orders, to meet their month-end quotas.
+Show all orders made on the last day of the month. Order by EmployeeID and OrderID.
+*/
