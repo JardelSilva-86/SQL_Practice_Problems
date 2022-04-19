97f1f733ac1a18871aba66231e02f0f9fa51b723
JardelSilva <53876124+JardelSilva-86@users.noreply.github.com>
2022-04-19 14:37:23 -0300

    Create Exercicio38.sql

---
+++ 3_AdvancedChallenges/Exercicio38.sql
@@ -0,0 +1,7 @@
+/* Exercise 38 - Orders - accidental double entry
+
+Janet Leverling, one of the salespeople, has come to you with a request.
+She thinks that she accidentally double-entered a line item on an order, with a different ProductID, but the same quantity.
+She remembers that the quantity was 60 or more. 
+Show all the OrdersIDs with line items that match this, in order of OrdersID.
+*/
