79a16fbfae490dd5e67eaa31692aac0391b54b32
JardelSilva <53876124+JardelSilva-86@users.noreply.github.com>
2022-04-19 14:26:55 -0300

    Create Exercicio36.sql

---
+++ 3_AdvancedChallenges/Exercicio36.sql
@@ -0,0 +1,5 @@
+/* Exercise 36 - Orders with many line items
+
+The Northwind mobile app developers are testing an app that customers will use to show orders. In order to make sure that even the largest orders will show up correctly on the app, they'd like some samples of orders that havelots of individual line items.
+Show the 10 orders with the most line items, in order of total line items.
+*/
