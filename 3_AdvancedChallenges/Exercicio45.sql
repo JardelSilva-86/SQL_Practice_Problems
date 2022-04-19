73b81e3fca6d884768d7d6c54716d34986d4468a
JardelSilva <53876124+JardelSilva-86@users.noreply.github.com>
2022-04-19 15:24:03 -0300

    Create Exercicio45.sql

---
+++ 3_AdvancedChallenges/Exercicio45.sql
@@ -0,0 +1,5 @@
+/* Exercise 45 - Late orders vs. total orders - fix null
+
+Continuing on the answer for above query, let's fix the results for row 5 - Buchanan.
+He should have a 0 instead of a Null in LateOrders.
+*/
