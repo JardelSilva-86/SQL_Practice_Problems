bf9c8a1b3a382c75a57ed749102b560f61c67ce7
JardelSilva <53876124+JardelSilva-86@users.noreply.github.com>
2022-04-19 15:16:11 -0300

    Create Exercicio43.sql

---
+++ 3_AdvancedChallenges/Exercicio43.sql
@@ -0,0 +1,6 @@
+/* Exercise 43 - Late orders vs. total orders
+
+Andrew, the VP of sales, has been doing some more thinking some more about the problem of late orders.
+He realizes that just looking at the number of orders arrving late for each salesperson isn't a good idea.
+It needs to be compared against the total number of orders per salesperson. Return results like the following:
+*/
