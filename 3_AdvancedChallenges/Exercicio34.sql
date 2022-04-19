bb88a937cce7b94ee03bbeb9c6d0c27811a085e6
JardelSilva <53876124+JardelSilva-86@users.noreply.github.com>
2022-04-19 14:16:19 -0300

    Create Exercicio34.sql

---
+++ 3_AdvancedChallenges/Exercicio34.sql
@@ -0,0 +1,4 @@
+/* Exercise 34 - High-value customers - with discount
+
+Change the above query to use the discount when calculating high-value customers. Order by the total amount which includes the discount.
+*/
