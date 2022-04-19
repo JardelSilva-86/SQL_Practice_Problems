b93a031b7519bd534005c7e2791faa54731da050
JardelSilva <53876124+JardelSilva-86@users.noreply.github.com>
2022-04-19 14:33:47 -0300

    Create Exercicio37.sql

---
+++ 3_AdvancedChallenges/Exercicio37.sql
@@ -0,0 +1,5 @@
+/* Exercise 37 - Orders - random assortment
+
+The northwind mobile app developers would now like to just get a random assortment of orders for beta testing on their app.
+Show a random set of 2% of all orders.
+*/
