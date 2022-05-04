e625537a286673355ce3fca51f7ab9fb9aebc89d
Jardel Silva <jardel.s22@gmail.com>
2022-05-04 20:15:23 -0300

    Update Exercicio27.sql

--- 2_IntermediateChallenges/Exercicio27.sql
+++ 2_IntermediateChallenges/Exercicio27.sql
@@ -1,10 +1,19 @@
-/* Exercise 27 - 
+/* Exercise 27 - High freight charges with between
 
+Another (incorrect) answer to the problem aboveis this:
 */
 
 SELECT TOP (3) ShipCountry,
   AVG(Freight) AS 'AVGFreight'
 FROM Orders
-WHERE OrderDate BETWEEN CONVERT(date, 1997-01-01 00:00:00.000 AND 1997-12-31 00:00:00.000)
+WHERE OrderDate BETWEEN '19970101' AND '19971231'
+GROUP BY ShipCountry
+ORDER BY AVGFreight DESC;
 
-Not Finished
+/*
+Notice when you run this, it gives Sweden as the ShipCountry with the third highest freight charges. 
+However, this is wrong - it should be France.
+What is the OrderID of the order that the (incorrect) answer above is missing?
+*/
+
+
