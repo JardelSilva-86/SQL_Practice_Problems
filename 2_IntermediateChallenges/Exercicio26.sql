346b2f15173d24f5f2455718e573ddcad64fe160
Jardel Silva <jardel.s22@gmail.com>
2022-05-04 20:15:15 -0300

    Update Exercicio26.sql

--- 2_IntermediateChallenges/Exercicio26.sql
+++ 2_IntermediateChallenges/Exercicio26.sql
@@ -1,5 +1,8 @@
-/* Exercise 26 - 
+/* Exercise 26 - High freight charges - 1997
 
+We're continuing on the question above on high freight charges.
+Now, instead of using all the orders we have, we only want to see
+orders from the year 1997.
 */
 
 SELECT TOP (3) ShipCountry,
@@ -8,3 +11,14 @@ FROM Orders
 WHERE YEAR(OrderDate) = 1997
 GROUP BY ShipCountry
 ORDER BY AVGFreight DESC;
+
+/*
+Another way to write this query
+*/
+SELECT TOP (3)
+	ShipCountry,
+	AVG(Freight) as 'AVGFreight'
+FROM Orders
+WHERE OrderDate >= '19970101' AND OrderDate < '19980101'
+GROUP BY ShipCountry
+ORDER BY AVGFreight DESC; 
